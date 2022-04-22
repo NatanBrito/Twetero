0ebe9b21ea0d92d2d9e554339d0c9287ce6d19cd
Natan__Brito <natanismaelbrito@gmail.com>
2022-04-22 11:28:59 -0300

    make-it

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
-
+import chalk from "chalk"
 const app= express();
 app.use(cors());
-app.listen(5000)
+app.listen(5000,console.log(chalk.bold.green("Silencio, estamos no AR!!!")))
+app.get("/vapo",(req,res)=>{
+    res.send("tamo ON MERMAO")
+})
